9613aa2ccb4593dc45f50a9e62c77e1389c74b9d
José Jairán Breijo Rodríguez <jose.breijo@wizeline.com>
2018-10-25 23:25:01 +0700

    Add Spinner component

---
+++ src/components/Spinner/Spinner.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Spinner.css';
+
+const Spinner = () => (
+  <div className="Spinner-container">
+    <span className="Spinner" />
+  </div>
+);
+
+export default Spinner;
