5ac6e827776ac712ebfcb1389703dcd6f0adabcf
José Jairán Breijo Rodríguez <jose.breijo@wizeline.com>
2018-10-26 11:44:34 +0700

    Remove caption from gif

--- src/components/Gif/Gif.component.js
+++ src/components/Gif/Gif.component.js
@@ -3,11 +3,7 @@ import React from 'react';
 const Gif = ({ info }) => {
   const title = info.data.title;
   const imageUrl = info.data.images.original.url;
-  return (
-    <figure>
-      <img src={imageUrl} alt={title} /> <figcaption>{title}</figcaption>
-    </figure>
-  );
+  return <img src={imageUrl} alt={title} />;
 };
 
 export default Gif;
